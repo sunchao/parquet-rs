bc48b38cec95b336a5ba3353c30d40296cc73ee9
Chao Sun <sunchao@apache.org>
2017-02-02 22:58:03 -0800

    Rename src/thrift to src/parquet_thrift
    To avoid name collision with the thrift crate.

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,14 @@
 #[macro_use]
 extern crate quick_error;
 extern crate byteorder;
+extern crate thrift;
+extern crate ordered_float;
+extern crate try_from;
 
 #[macro_use]
 pub mod errors;
-
 pub mod basic;
-
+pub mod parquet_thrift;
 pub mod schema;
 pub mod file;
 
